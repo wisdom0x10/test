d8a2f692c8b3f4d343aaab5fb3663879880f60b0
wr <wsidom.0x10@gmail.com>
2024-05-06 17:04:45 +0800

    init

--- packages/shared/rollup.config.js
+++ packages/shared/rollup.config.js
@@ -4,12 +4,21 @@ import terser from '@rollup/plugin-terser'
 
 export default {
   input: ['src/index.ts'],
-  output: {
-    dir: 'dist',
-    format: 'esm',
-    preserveModules: true,
-    banner: '// Create by wisdom0x10'
-    // plugins: [terser()]
-  },
+  output: [
+    {
+      dir: 'dist',
+      format: 'esm',
+      preserveModules: true,
+
+      banner: '// Create by wisdom0x10'
+    },
+    {
+      dir: 'dist',
+      format: 'esm',
+      entryFileNames: '[name].min.js',
+      preserveModules: true,
+      plugins: [terser()]
+    }
+  ],
   plugins: [clear({ targets: ['dist'] }), typescript()]
 }
